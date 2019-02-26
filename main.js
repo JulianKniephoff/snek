9ec89232132844c5d7d74e2e21e2c420fada235b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-26 21:16:45 +0000

    Deploy JulianKniephoff/snek to github.com/JulianKniephoff/snek.git:gh-pages

--- main.js
+++ main.js
@@ -348,7 +348,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"./pkg/snek_bg.wasm":"74ac7ccf743c275a07ef"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"./pkg/snek_bg.wasm":"6d39afa4ec5b9cc9e7b5"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
