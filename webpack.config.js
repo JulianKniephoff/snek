d2972b61fa824a4c1933163a07b30e5151def902
Julian Kniephoff <me@juliankniephoff.com>
2021-09-11 17:36:59 +0200

    Be explicit about Webpack's `mode`

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
 
 module.exports = {
+    mode: 'none',
     plugins: [
         new WasmPackPlugin({
             crateDirectory: '.',
